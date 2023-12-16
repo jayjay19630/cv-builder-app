280d8577f2e3f53792755013be9353c34573b065
positivelyjon <positivelyjon@gmail.com>
2023-12-16 15:06:12 +0800

    Create header with logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+function Header() {
+    return (
+        <div className="header">
+            <img className="logo" src="src/assets/images/logo.png"></img>
+            <p className="header-text">YOUR PROFESSIONAL CV MAKER</p>
+        </div>
+    )
+}
+
+export default Header;
