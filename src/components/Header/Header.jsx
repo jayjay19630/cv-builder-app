af03d1849f370ef3543d935c1b118bc273165bb1
positivelyjon <positivelyjon@gmail.com>
2023-12-16 15:47:51 +0800

    Create delete and load sample buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,14 @@ import "./Header.css";
 function Header() {
     return (
         <div className="header">
-            <img className="logo" src="src/assets/images/logo.png"></img>
-            <p className="header-text">YOUR PROFESSIONAL CV MAKER</p>
+            <div className="left-header">
+                <img className="logo" src="src/assets/images/logo.png"></img>
+                <p className="header-text">YOUR PROFESSIONAL CV MAKER</p>
+            </div>
+            <div className="right-header">
+                <button className="delete-content">Delete Content</button>
+                <button className="load-sample">Load Sample</button>
+            </div>
         </div>
     )
 }
