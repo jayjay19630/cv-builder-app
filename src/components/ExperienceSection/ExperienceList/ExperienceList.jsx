a4b5ec7c7b180249ba0e35ad52853c44403ce988
positivelyjon <positivelyjon@gmail.com>
2024-02-18 02:42:19 +0800

    Add recommended tags

--- src/components/ExperienceSection/ExperienceList/ExperienceList.jsx
+++ src/components/ExperienceSection/ExperienceList/ExperienceList.jsx
@@ -1,23 +1,39 @@
 import SingleFormInput from "../../ui/SingleFormInput/SingleFormInput";
-import './experiencelist.css'
+import "./experiencelist.css";
 
 export const ExperienceList = (props) => {
-
-    if (props.experienceData.length === 0) {
-        return (<></>);
-    }
-    return (
-        <ul className="experience-list">
-            {props.experienceData.map((experience, index) => 
-                <li key={index} className="experience-list-item">
-                    <SingleFormInput formTitle={"Occupation"} placeholder="Occupation"></SingleFormInput>
-                    <SingleFormInput formTitle={"Location"} placeholder="Location"></SingleFormInput>
-                    <SingleFormInput formTitle={"Company"} placeholder="Company"></SingleFormInput>
-                    <SingleFormInput formTitle={"Start Date"} placeholder="Start Date"></SingleFormInput>
-                    <SingleFormInput formTitle={"End Date"} placeholder="End Date"></SingleFormInput>
-                    <div className="divider"></div>
-                </li>
-            )}
-        </ul>
-    )
-}
+  if (props.experienceData.length === 0) {
+    return <></>;
+  }
+  return (
+    <ul className="experience-list">
+      {props.experienceData.map((experience, index) => (
+        <li key={index} className="experience-list-item">
+          <SingleFormInput
+            formTitle={"Occupation"}
+            recommendation={"Recommended"}
+            placeholder="Occupation"
+          ></SingleFormInput>
+          <SingleFormInput
+            formTitle={"Location"}
+            placeholder="Location"
+          ></SingleFormInput>
+          <SingleFormInput
+            formTitle={"Company"}
+            recommendation={"Recommended"}
+            placeholder="Company"
+          ></SingleFormInput>
+          <SingleFormInput
+            formTitle={"Start Date"}
+            placeholder="Start Date"
+          ></SingleFormInput>
+          <SingleFormInput
+            formTitle={"End Date"}
+            placeholder="End Date"
+          ></SingleFormInput>
+          <div className="divider"></div>
+        </li>
+      ))}
+    </ul>
+  );
+};
